1ef9186b21fc8bbfe46cf3bc9157f97ce59bf303
Gareth Barnard <1058419+gjb2048@users.noreply.github.com>
2021-07-16 16:31:31 +0100

    Week 4 task 1.

--- view.php
+++ view.php
@@ -48,10 +48,6 @@ require_login();
 // Check the users permissions to see the view page.
 require_capability('block/superframe:seeviewpage', $context);
 
-// Start output to browser.
-echo $OUTPUT->header();
-echo $OUTPUT->heading(get_string('pluginname', 'block_superframe'), 5);
-
 /* Get the instance configuration data from the database.
    It's stored as a base 64 encoded serialized string. */
 $configdata = $DB->get_field('block_instances', 'configdata', ['id' => $blockid]);
@@ -89,15 +85,5 @@ switch ($config->size) {
         break;
 }
 
-// Build and display an iframe.
-$attributes = [
-    'src' => $config->url,
-    'width' => $config->width,
-    'height' => $config->height,
-];
-
-echo html_writer::start_tag('iframe', $attributes);
-echo html_writer::end_tag('iframe');
-
-// Send footer out to browser.
-echo $OUTPUT->footer();
+$renderer = $PAGE->get_renderer('block_superframe');
+$renderer->display_view_page($url, $width, $height);
